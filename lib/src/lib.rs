cd322bb9d806b2a63dce25e3b6bede89d1ee4c25
Tim Jones <timjonez@protonmail.com>
2025-06-10 21:57:26 -0700

    create u256 type

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,8 @@
+use uint::construct_uint;
+construct_uint! {
+    // costruct an unsigned 256-bit integer consisting of 4 x 64-bit words
+    pub struct U256(4);
+}
 pub mod crypto;
 pub mod sha256;
 pub mod types;
